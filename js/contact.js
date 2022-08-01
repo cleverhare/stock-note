dc141a1f780dbcf11acc02cc466b05e89ab33904
soumyajit <soumyajitbrah@gmail.com>
2022-08-01 16:25:57 +0530

    Made in animated looking with new cursor

--- js/contact.js
+++ js/contact.js
@@ -186,3 +186,11 @@ function sendEmail(params) {
   }
    
 }
+// var cursor = document.querySelector('.cursor');
+var cursorinner = document.querySelector('.cursor2');
+document.addEventListener('mousemove', function(e){
+    var x = e.clientX;
+    var y = e.clientY;
+    cursorinner.style.left = x + 'px';
+    cursorinner.style.top = y + 'px';
+  });
