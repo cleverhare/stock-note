09db3e2a5a583052593cc45a2bdc382c23e4c8ee
soumyajit <soumyajitbrah@gmail.com>
2022-08-01 14:51:18 +0530

    made the email look good

--- js/contact.js
+++ js/contact.js
@@ -16,72 +16,168 @@ function sendEmail(params) {
         To : 'soumyajitbrah@gmail.com',
         From : "soumyajitbrah@gmail.com",
         Subject : `This is a query from ${val1}`,
-        Body : `Hi soumyajit brahma, This is a Query from mr/mrs ${val1}, From the Email ${emailval}, His/her query about the application is: ${message}`
+        Body : `<!doctype html>
+        <html lang="en-US">
+        
+        <head>
+            <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+            <title>Reset Password Email Template</title>
+            <meta name="description" content="Reset Password Email Template.">
+            <style type="text/css">
+                a:hover {text-decoration: underline !important;}
+            </style>
+        </head>
+        
+        <body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+            <!--100% body table-->
+            <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+                style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+                <tr>
+                    <td>
+                        <table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0"
+                            align="center" cellpadding="0" cellspacing="0">
+                            <tr>
+                                <td style="height:80px;">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td style="text-align:center;">
+                                  <a href="https://rakeshmandal.com" title="logo" target="_blank">
+                                    <img width="160" src="https://stock-note.netlify.app/media/Logo.png" title="logo" alt="logo">
+                                  </a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="height:20px;">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+                                        style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+                                        <tr>
+                                            <td style="height:40px;">&nbsp;</td>
+                                        </tr>
+                                        <tr>
+                                            <td style="padding:0 35px;">
+                                                <h1 style="color:#20e277; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">Name: ${val1}</h1>
+                                                <span
+                                                    style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+                                                <p style="color:#455056; font-size:15px;line-height:24px; margin:0;">
+                                                His/Her Email is:- ${emailval}
+                                                Message:- ${message}
+                                                </p>
+                                                <a traget="_blank" href="https://stock-note.netlify.app/"
+                                                    style="background:#20e277;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">
+                                                    Go to Stock Notes</a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td style="height:40px;">&nbsp;</td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            <tr>
+                                <td style="height:20px;">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td style="text-align:center;">
+                                    <p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; <strong>https://stock-note.netlify.app/</strong></p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="height:80px;">&nbsp;</td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+            <!--/100% body table-->
+        </body>
+        
+        </html>`
     })
     Email.send({
         SecureToken : "2d08c861-cd0f-40af-9793-58a518c9040d",
         To : `${emailval}`,
         From : "soumyajitbrah@gmail.com",
         Subject : `Hi ${val1}, Welcome to Stock-Note`,
-        Body : `<table width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
-
-        <!-- START HEADER/BANNER -->
+        Body : `
+        <!doctype html>
+        <html lang="en-US">
         
-            <tbody><tr>
-              <td align="center">
-                <table class="col-600" width="600" border="0" align="center" cellpadding="0" cellspacing="0">
-                  <tbody><tr>
-                    <td align="center" valign="top" background="https://images.unsplash.com/photo-1580927752452-89d86da3fa0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y29kaW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" bgcolor="#66809b" style="background-size:cover; background-position:top;height=" 400""="">
-                      <table class="col-600" width="600" height="400" border="0" align="center" cellpadding="0" cellspacing="0">
+        <head>
+            <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+            <title>Reset Password Email Template</title>
+            <meta name="description" content="Reset Password Email Template.">
+            <style type="text/css">
+                a:hover {text-decoration: underline !important;}
+            </style>
+        </head>
         
-                        <tbody><tr>
-                          <td height="40"></td>
-                        </tr>
-        
-        
-                        <tr>
-                          <td align="center" style="line-height: 0px;">
-                            <img style="display:block; line-height:0px; font-size:0px; border:0px;" src="https://stock-note.netlify.app/media/Logo.png" width="209" height="203" alt="logo">
-                          </td>
-                        </tr>
-        
-        
-        
-                        <tr>
-                          <td align="center" style="font-family: 'Raleway', sans-serif; font-size:37px; color:#ffffff; line-height:24px; font-weight: bold; letter-spacing: 7px;">
-                            Stock <span style="font-family: 'Raleway', sans-serif; font-size:37px; color:#ffffff; line-height:39px; font-weight: 300; letter-spacing: 7px; font-weight: bold;">Notes</span>
-                          </td>
-                        </tr>
-        
-        
-        
-        
-        
-                        <tr>
-                          <td align="center" style="font-family: 'Lato', sans-serif; font-size:15px; color:#ffffff; line-height:24px; font-weight: 300;">
-                          Hi, ${val1} we are here to welcome you this time. Hope your Job workflow will get a higher mark using stock-note. So if you have any query related to this email or anything regarding stock-note. Reach us at soumyajitbrah@gmail.com
-
-                          </td>
-                        </tr>
-        
-        
-                        <tr>
-                          <td height="50"></td>
-                        </tr>
-                      </tbody></table>
+        <body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+            <!--100% body table-->
+            <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+                style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+                <tr>
+                    <td>
+                        <table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0"
+                            align="center" cellpadding="0" cellspacing="0">
+                            <tr>
+                                <td style="height:80px;">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td style="text-align:center;">
+                                  <a href="https://rakeshmandal.com" title="logo" target="_blank">
+                                    <img width="160" src="https://stock-note.netlify.app/media/Logo.png" title="logo" alt="logo">
+                                  </a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="height:20px;">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+                                        style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+                                        <tr>
+                                            <td style="height:40px;">&nbsp;</td>
+                                        </tr>
+                                        <tr>
+                                            <td style="padding:0 35px;">
+                                                <h1 style="color:#20e277; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">Welcome ${val1}, to Stock Notes</h1>
+                                                <span
+                                                    style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+                                                <p style="color:#455056; font-size:15px;line-height:24px; margin:0;">
+                                                Hi, ${val1} we are here to welcome you this time. Hope your Job workflow will get a higher mark using stock-note. So if you have any query related to this email or anything regarding stock-note. Reach us at soumyajitbrah@gmail.com
+                                                </p>
+                                                <a traget="_blank" href="https://stock-note.netlify.app/"
+                                                    style="background:#20e277;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">
+                                                    Go to Stock Notes</a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td style="height:40px;">&nbsp;</td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            <tr>
+                                <td style="height:20px;">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td style="text-align:center;">
+                                    <p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; <strong>https://stock-note.netlify.app/</strong></p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="height:80px;">&nbsp;</td>
+                            </tr>
+                        </table>
                     </td>
-                  </tr>
-                </tbody></table>
-              </td>
-            </tr>
+                </tr>
+            </table>
+            <!--/100% body table-->
+        </body>
         
-        
-        <!-- END HEADER/BANNER -->
-        
-       
-        
-        
-       `
+        </html>`
     })
   } else {
     let status = document.querySelector('.status')
